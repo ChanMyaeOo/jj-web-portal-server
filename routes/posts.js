366f4527106300bde72fdb13585f020af3450f07
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-10-12 23:16:51 +0630

    added delete post operation

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPost, getPosts, getPost, updatePost } from '../controllers/Posts.js'
+import { createPost, getPosts, getPost, updatePost, deletePost } from '../controllers/Posts.js'
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post('/', createPost)
 router.get('/', getPosts)
 router.get('/:id', getPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router
