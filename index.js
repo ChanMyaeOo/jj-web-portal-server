52b1765ee016ac28d68098c45e6210b9598d06dc
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-10-19 19:47:02 +0630

    changed mongo url

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use('/posts', postRoutes)
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose.connect('mongodb+srv://jjwebportal:jjwebportal123@cluster0.i3yt3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: `, PORT)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
