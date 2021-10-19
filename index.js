74c3943689bc50bf108038fc4a371bd11ecd1f09
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-10-19 18:43:19 +0630

    make it better

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
+import dotenv from 'dotenv';
 
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: '30mb', extended: 'true'}))
 app.use(express.urlencoded({ limit: '30mb', extended: 'true'}))
@@ -12,8 +14,8 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = 'mongodb+srv://jjwebportal:jjwebportal123@cluster0.i3yt3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
